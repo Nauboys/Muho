fcefbb64fe648be95487c94c46a881575993de9b
Nauboys <58691715+Nauboys@users.noreply.github.com>
2020-09-08 13:50:49 +0400

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,8 +9,8 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://www.dropbox.com/s/mastxnd4trl3xnv/export_classifier.pkl?dl=0'
+export_file_name = 'export_classifier.pkl'
 
 classes = ['black', 'grizzly', 'teddys']
 path = Path(__file__).parent
